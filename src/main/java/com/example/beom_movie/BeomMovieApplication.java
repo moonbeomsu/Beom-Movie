154d082195f449a4132f8b195ad59839bc638ee4
moonbeomsu <980613mbs@gmail.com>
2023-06-28 19:41:28 +0900

    commit

--- src/main/java/com/example/beom_movie/BeomMovieApplication.java
+++ src/main/java/com/example/beom_movie/BeomMovieApplication.java
@@ -1,4 +1,4 @@
-package com.example.beommovie;
+package com.example.beom_movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/beom_movie/BeomMovieApplicationTests.java
+++ src/test/java/com/example/beom_movie/BeomMovieApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.beommovie;
+package com.example.beom_movie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
