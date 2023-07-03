63d96f2c8cc6165430bf0b7c684cd751932406e6
moonbeomsu <980613mbs@gmail.com>
2023-07-03 22:57:47 +0900

    1차 UI 및 기초 설정

--- src/main/java/com/example/beom_movie/BeomMovieApplication.java
+++ src/main/java/com/example/beom_movie/BeomMovieApplication.java
@@ -2,8 +2,10 @@ package com.example.beom_movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BeomMovieApplication {
 
     public static void main(String[] args) {
